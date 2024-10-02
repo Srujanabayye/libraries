de99ed0d807b9972bbc1bdcd74fe19c2fdbd836c
Srujana Bayye <111349810+Srujanabayye@users.noreply.github.com>
2024-10-02 22:13:46 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,15 @@
+def gitDownload(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}.git"
+}
+def buildArtifact()
+{
+  sh 'mvn package'
+}
+def deployTomcat(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
+def runSelenium(jobname)
+{
+  
